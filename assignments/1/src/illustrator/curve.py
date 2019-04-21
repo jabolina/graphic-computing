f333abe281fa3c6912dc567bf1155e5c9f93e395
jabolina <jose.bolina@neppoadm.com.br>
2019-04-21 13:27:35 -0300

    add bezier

--- assignments/1/src/illustrator/curve.py
+++ assignments/1/src/illustrator/curve.py
@@ -1,11 +1,22 @@
 from typing import List, Tuple
 
 import pygame
+from pygame.constants import MOUSEBUTTONDOWN, QUIT, KEYDOWN, MOUSEBUTTONUP
 
+from user_interface.constants import DEFAULT_DIMENSION, WHITE
+from user_interface.context import DrawContext
+from user_interface.utils import draw_to_surface
 from . import BaseIllustrator
 
 
+class PointContext:
+    def __init__(self, coordinates):
+        self.x = coordinates[0]
+        self.y = coordinates[1]
+
+
 class CurveIllustrator(BaseIllustrator):
+
     @staticmethod
     def compute_bezier_points(vertices, num_points=30):
         result = []
@@ -63,12 +74,21 @@ class CurveIllustrator(BaseIllustrator):
 
         return result
 
-    def draw_bezier(self, control_points: List[Tuple]):
+    def _draw_bezier(self, control_points: List[Tuple], surface: pygame.Surface):
         b_points = CurveIllustrator.compute_bezier_points([x for x in control_points])
-        pygame.draw.lines(self.gui_context.screen, self.options_context.line_color, False, b_points, 2)
+        pygame.draw.lines(surface, self.options_context.line_color, False, b_points, 2)
 
         # Flip screen
         pygame.display.flip()
+
+    def draw(self, control_points: List[Tuple]) -> DrawContext:
+        bezier_surface = pygame.Surface(DEFAULT_DIMENSION, pygame.SRCALPHA, 32)
+        bezier_surface.convert_alpha()
+        self._draw_bezier(control_points, bezier_surface)
+        bezier_context = DrawContext(bezier_surface, (0, 0))
+        bezier_context.is_valid = False
+
+        return bezier_context
 
     def draw_line(self, *args, **kwargs):
         """
@@ -79,8 +99,11 @@ class CurveIllustrator(BaseIllustrator):
         :param args: anything
         :param kwargs: anything
         """
-        control_points = []
+        control_points: List[PointContext] = []
         original_position = kwargs.get('option_position')
+        is_running = True
+        selected: PointContext = None
+        clock = pygame.time.Clock()
 
         while len(control_points) < 4:
             event = pygame.event.wait()
@@ -89,9 +112,38 @@ class CurveIllustrator(BaseIllustrator):
                 is_pressed, _, __ = pygame.mouse.get_pressed()
 
                 if is_pressed and mouse_position != original_position:
-                    control_points.append(mouse_position)
+                    control_points.append(PointContext(mouse_position))
 
-        self.draw_bezier(control_points)
+        lightgray = (200, 200, 200)
+        green = (0, 255, 0)
+        blue = (0, 0, 255)
+        while is_running:
+            context = self.draw([(p.x, p.y) for p in control_points])
+            bezier_surface = context.element
+
+            for event in pygame.event.get():
+                if event.type in (QUIT, KEYDOWN):
+                    is_running = False
+                elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+                    for p in control_points:
+                        if abs(p.x - event.pos[0]) < 10 and abs(p.y - event.pos[1]) < 10:
+                            selected = p
+                elif event.type == MOUSEBUTTONUP and event.button == 1:
+                    selected = None
+
+            if selected is not None:
+                selected.x, selected.y = pygame.mouse.get_pos()
+                pygame.draw.circle(bezier_surface, green, (selected.x, selected.y), 10)
+
+            for p in control_points:
+                pygame.draw.circle(bezier_surface, blue, (p.x, p.y), 10)
+
+            pygame.draw.lines(bezier_surface, lightgray, False, [(p.x, p.y) for p in control_points])
+            draw_to_surface(self.gui_context.screen, [context, *self.gui_context.draw_surfaces])
+
+            clock.tick(100)
+
+        self.gui_context.draw_surfaces = self.draw([(p.x, p.y) for p in control_points])
 
     def draw_circle(self, *args, **kwargs):
         print("Draw circle")
